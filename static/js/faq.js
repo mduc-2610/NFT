ad4e9ca8be23ed0de61387650a1ceb370f9f68c5
Duckyyy <ducdang26102003@gmail.com>
2023-10-16 22:08:12 +0700

    80% complete home1 home2 home4 home5 templates

--- static/js/faq.js
+++ static/js/faq.js
@@ -3,10 +3,14 @@ faqButton.forEach(function (button) {
     const faqPlus = button.children[0];
     const faqMinus = button.children[1];
     const faqAnswer = button.nextElementSibling.children[1];
+    const faqItem = button.parentElement;
+    console.log(faqItem);
     button.addEventListener("click", function () {
         faqAnswer.classList.toggle("show");
         faqMinus.classList.toggle("show-minus");
         faqPlus.classList.toggle("show-plus");
+        faqItem.classList.toggle("change-border-color");
+        button.style.transition = "max-height 0.4s ease;";
         console.log("asdasddasdsd")
     });
 });
