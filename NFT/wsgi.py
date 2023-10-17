114c1a9c056e3267330e8f4466360d0e12a70643
Duckyyy <ducdang26102003@gmail.com>
2023-10-17 17:52:46 +0700

    for hosting

--- NFT/wsgi.py
+++ NFT/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for NFT project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = os.path.expanduser('~/django_projects/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NFT.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
