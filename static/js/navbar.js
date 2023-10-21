abb2a444ac96cc6bd7c4b6aa2aa0bc5e6ba805b9
Duckyyy <ducdang26102003@gmail.com>
2023-10-21 15:53:47 +0700

    complete navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -41,26 +41,25 @@ const headerSearchInput = document.querySelector('.header__search-input');
 const navbarList = document.querySelector('.navbar__list');
 const headerMenuBack = document.querySelector('.header__menu-back');
 
-function getWindowWidth() {
-    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        console.log("Window width: " + width + " pixels");
-}
-var prevSize = "";
-headerSearchInput.addEventListener('click', (e) => {
-    prevSize = window.getComputedStyle(headerSearchInput.parentElement).getPropertyValue('width');
-    headerSearchInput.parentElement.style.width = "70%";
-    navbarList.style.display = 'none';
-    headerMenuBack.style.display = 'block';
-});
+let prevSize = "";
 console.log("Previous size" + prevSize + "*********" + window.getComputedStyle(headerSearchInput.parentElement).getPropertyValue('width'));
 window.addEventListener("resize", (e) => {
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if(width <= 968 || window.innerWidth <= 968) {
+        headerSearchInput.addEventListener('click', (e) => {
+            prevSize = window.getComputedStyle(headerSearchInput.parentElement).getPropertyValue('width');
+            headerSearchInput.parentElement.style.width = "70%";
+            navbarList.style.display = 'none';
+            headerMenuBack.style.display = 'block';
+        });
+    }
     headerMenuBack.addEventListener('click', (e) => {
-        headerSearchInput.parentElement.style.width = prevSize;
+        headerSearchInput.parentElement.style.width = (width <= 968 || window.innerWidth <= 968) ? "20%" : "40%" ;
         navbarList.style.display = 'flex';
         headerMenuBack.style.display = 'none';
     });
 });
+
 
 const connectButton = document.querySelectorAll('.header-login__button');
 const connect = document.querySelector('.connect');
@@ -88,6 +87,34 @@ connectCloseButton.onclick = (e) => {
 };
 
 
+const headerSearchButtonMobile1 = document.querySelector('.header__search-button-mobile-1');
+const headerMenuBackMobile = document.querySelector('.header__menu-back-mobile');
+const headerSearchBarMobile = document.querySelector('.header__search-bar-mobile');
+const headerLogoImg = document.querySelector(".header__logo-img");
+
+headerSearchButtonMobile1.onclick = (e) => {
+    headerSearchButtonMobile1.style.display = 'none';
+    headerSearchBarMobile.style.display = 'flex';
+    headerMenuBackMobile.style.display = 'block';
+    headerLogoImg.style.display = 'none';
+}
+
+// document.querySelector('body').onclick = (e) => {
+//     console.log(e.target)
+//     if(e.target !== headerSearchBarMobile) {
+//         headerSearchButtonMobile1.style.display = 'block';
+//         headerSearchBarMobile.style.display = 'none';
+//         headerMenuBackMobile.style.display = 'none';
+//     }
+// }
+
+headerMenuBackMobile.onclick = (e) => {
+    // e.stopPropagation();
+    headerSearchButtonMobile1.style.display = 'block';
+    headerSearchBarMobile.style.display = 'none';
+    headerMenuBackMobile.style.display = 'none';
+    headerLogoImg.style.display = 'inline-block';
+}
     // const connect = document.querySelector(".connect");
     // connect.addEventListener('click', (e) => {
     //   e.stopPropagation();
